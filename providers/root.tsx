82444a034dd6b99e6d87acaee52c2056250af897
characterMi <abol1385fx@gmail.com>
2025-05-28 21:31:09 +0330

    adding offline support

--- providers/root.tsx
+++ providers/root.tsx
@@ -9,9 +9,26 @@ const Root = ({
 }>) => {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/sw.js").catch(() => {
-        console.error("Service worker registration failed!");
-      });
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then(() => {
+          if (navigator.serviceWorker.controller) {
+            navigator.serviceWorker.controller.postMessage({
+              type: "CACHE-MISSING-ASSETS",
+            });
+          } else {
+            navigator.serviceWorker.ready.then(() => {
+              if (navigator.serviceWorker.controller) {
+                navigator.serviceWorker.controller.postMessage({
+                  type: "CACHE-MISSING-ASSETS",
+                });
+              }
+            });
+          }
+        })
+        .catch((error) => {
+          console.error("Service worker registration failed:", error);
+        });
     }
   }, []);
 
