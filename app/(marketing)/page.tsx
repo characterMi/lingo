241443eb33f60fcc961e2a69bfd3494972409131
characterMi <abol1385fx@gmail.com>
2025-07-09 22:08:32 +0330

    adding the sizes property to the hero image

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,7 +5,14 @@ export default function Home() {
   return (
     <div className="max-w-[988px] mx-auto flex-1 flex flex-col lg:flex-row w-full items-center justify-center p-4 gap-2">
       <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
-        <Image src="/hero.webp" priority fill alt="" loading="eager" />
+        <Image
+          src="/hero.webp"
+          priority
+          fill
+          alt=""
+          loading="eager"
+          sizes="(max-width:640px) 640w, (max-width:750px) 750w, (max-width:1080px) 1080w, (max-width:1200px) 1200w, (max-width:1920px) 1920w, (max-width:3840px) 3840w"
+        />
       </div>
 
       <div className="flex flex-col items-center gap-y-8">
