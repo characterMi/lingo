86edd7113e1d3cb9300b0becfc3a5e3f0d74d8dc
characterMi <abol1385fx@gmail.com>
2025-08-03 15:29:16 +0330

    Update MobileHeaderContainer.tsx

--- components/MobileHeaderContainer.tsx
+++ components/MobileHeaderContainer.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 const MobileHeaderContainer = ({ children }: { children: React.ReactNode }) => {
   const [backgroundColor, setBackgroundColor] = useState("bg-green-500");
@@ -9,45 +10,60 @@ const MobileHeaderContainer = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     const handlePageScroll = () => {
-      const greenUnitSections =
-        document.querySelectorAll<HTMLDivElement>(".unit-section__1")!;
-      const purpleUnitSections =
-        document.querySelectorAll<HTMLDivElement>(".unit-section__2")!;
-      const emeraldUnitSections =
-        document.querySelectorAll<HTMLDivElement>(".unit-section__3")!;
-      const blueUnitSections =
-        document.querySelectorAll<HTMLDivElement>(".unit-section__4")!;
-      const orangeUnitSections =
-        document.querySelectorAll<HTMLDivElement>(".unit-section__5")!;
+      const themeMeta = document.querySelector<HTMLMetaElement>(
+        "meta[name=theme-color]"
+      );
+
+      if (pathName !== "/learn") {
+        themeMeta?.setAttribute("content", "#22c55e");
+        setBackgroundColor("bg-green-500");
+        return;
+      }
 
       const currentScrollPosition = window.scrollY;
 
+      const greenUnitSections =
+        document.querySelectorAll<HTMLDivElement>(".unit-section__1");
+      const purpleUnitSections =
+        document.querySelectorAll<HTMLDivElement>(".unit-section__2");
+      const emeraldUnitSections =
+        document.querySelectorAll<HTMLDivElement>(".unit-section__3");
+      const blueUnitSections =
+        document.querySelectorAll<HTMLDivElement>(".unit-section__4");
+      const orangeUnitSections =
+        document.querySelectorAll<HTMLDivElement>(".unit-section__5");
+
       greenUnitSections.forEach((item) => {
         if (currentScrollPosition >= item.offsetTop) {
+          themeMeta?.setAttribute("content", "#22c55e");
           setBackgroundColor("bg-green-500");
         }
       });
 
       purpleUnitSections.forEach((item) => {
         if (currentScrollPosition >= item.offsetTop) {
+          themeMeta?.setAttribute("content", "#c084fc");
           setBackgroundColor("bg-purple-400");
         }
       });
 
       emeraldUnitSections.forEach((item) => {
         if (currentScrollPosition >= item.offsetTop) {
+          themeMeta?.setAttribute("content", "#34d399");
           setBackgroundColor("bg-emerald-400");
         }
       });
 
       blueUnitSections.forEach((item) => {
         if (currentScrollPosition >= item.offsetTop) {
+          themeMeta?.setAttribute("content", "#38bdf8");
           setBackgroundColor("bg-sky-400");
         }
       });
 
       orangeUnitSections.forEach((item) => {
         if (currentScrollPosition >= item.offsetTop) {
+          themeMeta?.setAttribute("content", "#fb923c");
           setBackgroundColor("bg-orange-400");
         }
       });
@@ -58,13 +74,14 @@ const MobileHeaderContainer = ({ children }: { children: React.ReactNode }) => {
     return () => {
       window.removeEventListener("scroll", handlePageScroll);
     };
-  }, []);
+  }, [pathName]);
 
   return (
     <nav
-      className={`lg:hidden px-6 h-12 transition flex items-center ${
-        pathName === "/learn" ? backgroundColor : "bg-green-500"
-      } border-b fixed top-0 w-full z-50`}
+      className={twMerge(
+        "lg:hidden px-6 h-12 transition flex items-center border-b fixed top-0 w-full z-50",
+        backgroundColor
+      )}
     >
       {children}
     </nav>
