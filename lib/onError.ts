5722366217f03ffd0195f4759e99ed256c068e11
characterMi <abol1385fx@gmail.com>
2025-08-13 20:02:41 +0330

    Update onError.ts

--- lib/onError.ts
+++ lib/onError.ts
@@ -3,5 +3,5 @@ import { toast } from "sonner";
 export const onError = (message: string, error?: Error) => {
   toast.error(message);
   error && console.error(error);
-  navigator?.vibrate(200);
+  navigator.vibrate?.(200);
 };
